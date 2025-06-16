c82e913bd38d5e0aece49d285e985d3afd351e0e
grupo-2A <magoyenecheo@itc.edu.co>
2025-06-15 21:17:50 -0500

    actualizacion panel admin

---
+++ Backend/schemas/product.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+
+class ProductBase(BaseModel):
+    name: str
+    description: str
+
+class ProductCreate(ProductBase):
+    pass
+
+class Product(ProductBase):
+    id: int
+
+    class Config:
+        orm_mode = True
